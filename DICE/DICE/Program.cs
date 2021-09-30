a15ccd508c159083c61e683dbce008185d5017f5
lekhavan <91652143+lekhavan@users.noreply.github.com>
2021-09-30 19:00:28 +0700

    Create project and class "Dice"

---
+++ DICE/DICE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DICE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
